45af32f5c8322d6dde83de620f5bbebf16da5df3
Vikash59 <vikash059.cse14@gmail.com>
2017-09-03 21:52:22 +0530

    Deleted gecxpress1

--- Gec_Xpress1/admin/gecdp.php
+++
@@ -1,8 +0,0 @@
-<?php
-//$con=mysqli_connect("localhost","root","")or die("sorry!cannot conect");
-//        $db=mysqli_select_db("gec", $con);
-////        echo $con;
-////        echo"vikash";   
-////        echo $db;
-$con= mysqli_connect("localhost", "root", "", "gec") or die("Sorry!! Vikash can not connect");
-        ?>
