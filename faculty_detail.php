653d97dfa206fda99bf3fc85188a0fecd1150528
manmohan141295 <manmohan.chandrawanshi@gmail.com>
2017-09-08 16:09:49 +0530

    issue solve
    photo update

--- faculty_detail.php
+++ faculty_detail.php
@@ -1,3 +1,67 @@
+<?php 
+include './gecdp.php';
+$qry="SELECT * FROM faculty_profile ORDER BY fid";
+$result= mysqli_query($con, $qry);
+?>
+<!Doctype html>
+<html>
+    <head>
+        <title>Teachers Details</title>
+        <meta charset="utf-8">
+        <link rel="icon" href="images/bulb_logo.png"/>
+        <meta name="viewport" content="width=device-width, intial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <?php include './header.php'; ?>
+        <div class="container-fluid table-responsive" style="margin-top: 50px;margin-bottom: 50px;">
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>DEPARTMENT</th>
+                        <th>NAME</th>
+                        <th>PHOTO</th>
+                        <th>QUALIFICATION</th>
+                        <th>EXPERIENCE</th>
+                        <th>SUBJECT THOUGHT'S</th>
+                        <th>RESEARCH AREA</th>
+                        <th>MOBILE</th>
+                        <th>EMAIL-ID</th>
+                        <th>UPDATE</th>
+                        <th>DELETE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while($row= mysqli_fetch_array($result)){ ?>
+                    <tr>
+                        <td><?php echo $row["department"]; ?></td>
+                        <td><?php echo $row["fname"]; ?></td>
+                        <td><img src="facultyimage/<?php echo $row["image"] ?>" style="height: 50px; width: 50px;" class="img-responsive"/></td>                        
+                        <td><?php echo $row["qualification"]; ?></td>
+                        <td><?php echo $row["experience"]; ?></td>
+                        <td><?php echo $row["subject"]; ?></td>
+                        <td style="height: 20px;width: 60px"><?php echo $row["researcharea"]; ?></td>
+                        <td><?php echo $row["mobile"]; ?></td>
+                        <td><?php echo $row["email"]; ?></td>
+              <?php 
+              
+                  echo"<td><a href='faculty_register.php?id=".$row["fid"]."&status=edit' style='text-decoration:none'>Edit</a>";
+       echo"<td><a href='faculty_detail.php?id=".$row["fid"]."&status=delete' style='text-decoration:none; text-align:center'>Delete</a>";
+       ?>         
+                    
+                    </tr>
+                    <?php }?>
+                </tbody>
+            </table>
+        </div>
+        <?php include './footer.php'; ?>
+    </body>
+</html>
+
+
+<!-- DElete querying-->
 <?php
 if(isset($_REQUEST["id"]) && $_REQUEST["status"]=="delete")
 {
@@ -26,60 +90,3 @@ if(isset($_REQUEST["id"]) && $_REQUEST["status"]=="delete")
     }
 
 }?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-       
-        include './gecdp.php';
-$qry="SELECT * FROM faculty_profile ORDER BY fid";
-$result= mysqli_query($con, $qry);
-
-echo "<table width=100% border=1>
-<tr style='background-color:#FFCCCC'>
-<td width=70>ID</td>
-
-<td width=130>NAME</td>
-<td width=100>DEPARTMENT</td>
-<td width=120>QUALIFICATION</td>
-<td width=80>IMAGE</td>
-<td width=100>EXPERIENCE</td>
-<td width=80>SUBJECT</td>
-<td width=100>RESEARCH AREA</td>
-<td width=100>UPDATE</td>
-<td width=100>DELETE</td>
-</tr>";
-$c=1;
-while($row= mysqli_fetch_array($result))
-{
-    if($c%2==0)
-    {
-       echo "<tr style='background-color:#CCCCFF'>"; 
-    }
- else {
-       echo "<tr style='background-color:#CCCCFF'>"; 
-      }
-   echo"<td>".$row["fid"]."</td>"; 
-    echo"<td>".$row["fname"]."</td>"; 
-     echo"<td>".$row["department"]."</td>"; 
-      echo"<td>".$row["qualification"]."</td>"; 
-      ?>
-    <td> <img src="facultyimage/<?php echo $row["image"] ?>" style="height: 50px; width: 50px;"></td>  
-      <?php
-      //echo"<td><img src=facultyimgage/pasta.jpg></td>"; 
-      echo"<td>".$row["experience"]."</td>"; 
-        echo"<td>".$row["subject"]."</td>"; 
-        echo"<td>".$row["researcharea"]."</td>"; 
-        echo"<td><a href='faculty_register.php?id=".$row["fid"]."&status=edit' style='text-decoration:none'>Edit</a>";
-       echo"<td><a href='faculty_detail.php?id=".$row["fid"]."&status=delete' style='text-decoration:none; text-align:center'>Delete</a>";
-        echo "</tr>";
-      $c++;
-}
-echo "</table>";
-        ?>
-    </body>
-</html>
